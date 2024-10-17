ed173fe071a730e2bd601f89809cebf0c0a8e933
NojusGl <99596734+NojusGl@users.noreply.github.com>
2024-10-17 22:01:35 +0300

    first step
    requirements, testavimo failai, produkto registracija, funkcijų šablonai

--- e-shop/__init__.py
+++ e-shop/__init__.py
@@ -28,4 +28,59 @@ def create_app():
 
         return {"message": "Client is registered"}, 200
     
+    # put – Register a new product.
+    @app.route('/products', methods=['PUT'])
+    def reg_prod ():
+        reqBody = request.json
+        
+        id = reqBody.get("id")
+        name = reqBody.get("name")
+        category = reqBody.get("category")
+        price = reqBody.get("price")
+
+        if ((id is None) or (name is None) or (category is None) or (price is None)):
+            return {"message": "Invalid data, or some of the values are missing"}, 400
+        elif (products.find_one({"id": id})):
+            return {"message": "This ID is already taken"}, 400
+        else:
+            product = {
+                "id": id,
+                "name": name,
+                "category": category,
+                "price": price
+            }
+
+            products.insert_one(product)
+            return {"message": "Product is registered"}, 200
+
+    # get – List all products, optionally in category.
+    @app.route('/products', methods=['GET'])
+    def list_prod ():
+        pass
+
+    # get – Get product details
+    @app.route('/products/<productId>', methods=['GET'])
+    def get_prod_info ():
+        pass
+
+    # del – Delete a product
+    @app.route('/products/<productId>', methods=['DELETE'])
+    def del_prod ():
+        pass
+
+    # get – Get top 10 clients by number of orders placed.
+    @app.route('/statistics/top/clients', methods=['GET'])
+    def top_clients ():
+        pass
+
+    # get – Get top 10 products by total quantity ordered.
+    @app.route('/statistics/top/products', methods=['GET'])
+    def top_prod ():
+        pass
+
+    # post – Delete all data from the database
+    @app.route('/cleanup', methods=['POST'])
+    def cleanup ():
+        pass
+
     return app

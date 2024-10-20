841ab5a3465ca2fe271e0ec598daed6a0949076f
Dangreed <tikondrotas@gmail.com>
2024-10-20 17:18:33 +0300

    a lot of changes added tests, modified README and added initial functions and "links"

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# e-shop
- second non-relational databases lab
+# E-shop
+Second project of non-relational databases
+
+[E-shop API](https://mif-nosql-assignments.s3.eu-central-1.amazonaws.com/2024/mongodb/mongodb-1.html) - reference point
+> [!NOTE]
+> **run.sh** for linux/mac, **run.bat** - for windows machines
+
+[!TIP]
+To run python virtual environment for tests, use commands below:
+For windows:
+> python -m venv ./.venv
+> ./.venv/Scripts/activate.ps1
+> pip3 install -r requirements.txt
+> pytest ./test/test_api.py
+
+For linux/mac:
+> python -m venv ./.venv
+> source ./.venv/Scripts/activate.bat
+> pip3 install -r requirements.txt
+> pytest ./test/test_api.py
--- e-shop/__init__.py
+++ e-shop/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     products = db.products
     orders = db.orders
 
-    #Register a new client.
+    # Register a new client.
     @app.route('/clients', methods=['PUT'])
     def set_client():
         reqBody = request.json
@@ -28,4 +28,70 @@ def create_app():
 
         return {"message": "Client is registered"}, 200
     
+    # Get client details.
+    @app.route('/clients/<clientId>', methods=['GET'])
+    def get_client():
+        pass
+
+    # Delete a client and its associated orders.
+    @app.route('/clients/<clientId>', methods=['DEL'])
+    def delete_client():
+        pass
+
+    # Register a new product.  
+    @app.route('/products', methods=['PUT'])  
+    def set_product():
+        pass
+
+    # List all products, optionally in category.  
+    @app.route('/products', methods=['GET'])  
+    def get_products():
+        pass
+
+    # Get product details.
+    @app.route('/product/<productId>', methods=['GET'])
+    def get_product():
+        pass
+    
+    # Delete a product.
+    @app.route('/product/<productId>', methods=['DEL'])
+    def delete_product():
+        pass
+    
+    # Create a new order.
+    @app.route('/orders', methods=['PUT'])
+    def set_order():
+        pass
+
+    # Get client orders.
+    @app.route('/clients/<clientId>/orders', methods=['PUT'])
+    def get_client_orders():
+        pass
+    
+    # Get top 10 clients by number of orders placed.
+    @app.route('/statistics/top/clients', methods=['GET'])
+    def get_top10_clients_by_orders():
+        pass
+    
+    # Get top 10 products by total quantity ordered.
+    @app.route('/statistics/top/products', methods=['GET'])
+    def get_top10_products_by_ordered_quantity():
+        pass
+    
+    # Get number of orders placed.
+    @app.route('/statistics/orders/total', methods=['GET'])
+    def get_orders_count():
+        pass
+
+    # Get total value of orders placed.
+    @app.route('/statistics/orders/totalValue', methods=['GET'])
+    def get_orders_value():
+        pass
+
+    # Delete all data from the database.
+    @app.route('/cleanup', methods=['POST'])
+    def delete_database():
+        pass
+    
     return app
+
